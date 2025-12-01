d9fbe60dcd8138a65a8b6b8071219943be255a30
Liping Xu <xuliping@microsoft.com>
2025-12-01 00:45:16 +0000

    Add SAI_202505_COMPAT guards for port/queue trim packet statistics
    - SAI_PORT_STAT_DROPPED_TRIM_PACKETS
    - SAI_PORT_STAT_TX_TRIM_PACKETS
    - SAI_QUEUE_STAT_DROPPED_TRIM_PACKETS
    - SAI_QUEUE_STAT_TX_TRIM_PACKETS
    
    These statistics don't exist in SAI 202505.
    Also disable Nvidia trim stat calculation logic for 202505.

--- orchagent/portsorch.cpp
+++ orchagent/portsorch.cpp
@@ -305,8 +305,10 @@ const vector<sai_port_stat_t> port_stat_ids =
     SAI_PORT_STAT_IF_IN_FEC_CODEWORD_ERRORS_S15,
     SAI_PORT_STAT_IF_IN_FEC_CORRECTED_BITS,
     SAI_PORT_STAT_TRIM_PACKETS,
+#ifndef SAI_202505_COMPAT
     SAI_PORT_STAT_DROPPED_TRIM_PACKETS,
     SAI_PORT_STAT_TX_TRIM_PACKETS
+#endif
 };
 
 const vector<sai_port_stat_t> gbport_stat_ids =
@@ -344,8 +346,10 @@ static const vector<sai_queue_stat_t> queue_stat_ids =
     SAI_QUEUE_STAT_DROPPED_PACKETS,
     SAI_QUEUE_STAT_DROPPED_BYTES,
     SAI_QUEUE_STAT_TRIM_PACKETS,
+#ifndef SAI_202505_COMPAT
     SAI_QUEUE_STAT_DROPPED_TRIM_PACKETS,
     SAI_QUEUE_STAT_TX_TRIM_PACKETS
+#endif
 };
 static const vector<sai_queue_stat_t> voq_stat_ids =
 {
@@ -772,11 +776,16 @@ PortsOrch::PortsOrch(DBConnector *db, DBConnector *stateDb, vector<table_name_wi
     }
 
     // Nvidia custom trim stat calculation
+#ifndef SAI_202505_COMPAT
     if (isMlnxPlatform() && \
         isPortStatSupported(SAI_PORT_STAT_TRIM_PACKETS) && \
         isPortStatSupported(SAI_PORT_STAT_TX_TRIM_PACKETS) && \
         !isPortStatSupported(SAI_PORT_STAT_DROPPED_TRIM_PACKETS))
     {
+#else
+    if (false)
+    {
+#endif
         portStatPlugins += "," + nvdaPortTrimSha;
     }
 
--- tests/mock_tests/flexcounter_ut.cpp
+++ tests/mock_tests/flexcounter_ut.cpp
@@ -727,8 +727,10 @@ namespace flexcounter_test
         ASSERT_TRUE(checkFlexCounter(QUEUE_STAT_COUNTER_FLEX_COUNTER_GROUP, queueOid,
                                      {
                                          {QUEUE_COUNTER_ID_LIST,
+#ifndef SAI_202505_COMPAT
                                           "SAI_QUEUE_STAT_TX_TRIM_PACKETS,"
                                           "SAI_QUEUE_STAT_DROPPED_TRIM_PACKETS,"
+#endif
                                           "SAI_QUEUE_STAT_TRIM_PACKETS,"
                                           "SAI_QUEUE_STAT_DROPPED_BYTES,SAI_QUEUE_STAT_DROPPED_PACKETS,"
                                           "SAI_QUEUE_STAT_BYTES,SAI_QUEUE_STAT_PACKETS"
